1d2c94c8d536e6e9961340e2c7a6319c98776870
AdielAndradeR <adiel.andrade.r@gmail.com>
2019-09-16 12:53:42 -0300

    Modify main and add a data type transation

---
+++ lib/transaction.dart
@@ -0,0 +1,16 @@
+class Trasaction{
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+
+Trasaction({
+  this.id, 
+  this.title, 
+  this.amount,
+  this.date
+  });
+
+
+}
+
