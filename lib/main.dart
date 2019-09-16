d59c6a0fa3a6e6c6a4e89682bab0666702e69719
AdielAndradeR <adiel.andrade.r@gmail.com>
2019-09-16 15:43:11 -0300

    Splitting app to a widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import './widgets/user_transactions.dart';
 
-import './transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -16,27 +15,6 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-
-  final List<Transaction> transactions= [
-    Transaction(
-      id: 't1', 
-      title: 'New Shoes', 
-      amount: 169.99, 
-      date: DateTime.now()),
-      Transaction(
-      id: 't2', 
-      title: 'New Cap', 
-      amount: 49.99, 
-      date: DateTime.now()),
-      Transaction(
-      id: 't3', 
-      title: 'Subway', 
-      amount: 34.50, 
-      date: DateTime.now()),
-  ];
-
-  final titleControler = TextEditingController();
-  final amountControler = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -53,79 +31,9 @@ class MyHomePage extends StatelessWidget {
             elevation: 5,
             child:Text('Charts')),
           ),
-        Card(
-          child: Container(
-            padding: EdgeInsets.all(10),
-            child : Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: <Widget>[
-                TextField(
-                  decoration: InputDecoration(labelText: 'Title'),
-                  controller: titleControler,
-                ),
-                TextField(
-                  decoration: InputDecoration(labelText: 'Amount'),
-                  controller: amountControler,
-                ),
-                FlatButton(
-                  child: Text('Add Transaction'),
-                  textColor: Colors.blue,
-                  onPressed: () {
-                    
-                  },
-                )
-              ],
-            ),
-          )
-        ),
-        Column(
-          children: transactions.map((tx) {
-            return Card(
-              child: Row(children: <Widget>[
-                Container(
-                  margin: EdgeInsets.symmetric(
-                    vertical: 10, 
-                    horizontal: 15),
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.blue, 
-                      width: 2)
-                      ),
-                    
-                  padding: EdgeInsets.all(10),
-                  child: Text(
-                    'R\$${tx.amount.toStringAsFixed(2)}',
-                 
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.blue
-                    )
-                  ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                  Text(
-                    tx.title,
-                    style: TextStyle(
-                      fontSize: 18, 
-                      fontWeight: FontWeight.bold
-                    ),
-                  ),
-                  Text(
-                    DateFormat('dd-MM-yyyy').format(tx.date),
-                    style: TextStyle(
-                      color: Colors.grey
-                      )
-                    ,)
-                  ])
-
-                ]),
-            );
-          }).toList(),
-        )
-      ],)
+        UserTransaction()      
+        ],
+      )
     );
   }
 }
---
+++ lib/widgets/user_transactions.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import './new_transaction.dart';
+import './transaction_list.dart';
+import '../models/transaction.dart';
+
+class UserTransaction extends StatefulWidget {
+  @override
+  _UserTransactionState createState() => _UserTransactionState();
+}
+
+class _UserTransactionState extends State<UserTransaction> {
+
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      id: 't1', 
+      title: 'New Shoes', 
+      amount: 169.99, 
+      date: DateTime.now()),
+    Transaction(
+      id: 't2', 
+      title: 'New Cap', 
+      amount: 49.99, 
+      date: DateTime.now()),
+    Transaction(
+      id: 't3', 
+      title: 'Subway', 
+      amount: 34.50, 
+      date: DateTime.now()),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        NewTransaction(),
+        TransactionList(_userTransactions)
+      ],
+    );
+  }
+}
